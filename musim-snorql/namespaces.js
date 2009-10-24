65f545b55ec159c81705d9a0ad918b70e4dd3278
kurtjx <kurtjx@white-top-one.local>
2009-10-24 21:50:59 +0900

    adding musim verison of snorql for my convienice

---
+++ musim-snorql/namespaces.js
@@ -0,0 +1,17 @@
+var D2R_namespacePrefixes = {
+    rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+    rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
+    owl: 'http://www.w3.org/2002/07/owl#',
+    dc: 'http://purl.org/dc/elements/1.1/',
+    dcterms: 'http://purl.org/dc/terms/',
+    foaf: 'http://xmlns.com/foaf/0.1/',
+    tl: 'http://purl.org/NET/c4dm/timeline.owl#',
+    ev: 'http://purl.org/NET/c4dm/event.owl#',
+    sim: 'http://purl.org/ontology/similarity/',
+    mo: 'http://purl.org/ontology/mo/',
+    vamp: 'http://purl.org/ontology/vamp/',
+    af: 'http://purl.org/ontology/af/',
+    ov: 'http://open.vocab.org/terms/',
+    xsd: 'http://www.w3.org/2001/XMLSchema#',
+};
+
